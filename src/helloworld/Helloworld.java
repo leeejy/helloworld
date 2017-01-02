8cc4a529f58d22806a0e59fbaf0b257ddeb49336
JEONGYEON <JEONGYEON@DESKTOP-0UTTCSM>
2017-01-02 15:54:31 +0900

    1

---
+++ src/helloworld/Helloworld.java
@@ -0,0 +1,15 @@
+package helloworld;
+/**
+ * 
+ * @author JEONGYEON
+ *
+ */
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
